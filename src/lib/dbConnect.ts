ab421b170bce72ab05cfc1d440dfc7665c4c3ed0
Shuklashashi0706 <shuklashashi169@gmail.com>
2024-04-16 23:31:10 +0530

    [Database connection added]

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,2 +1,23 @@
 import mongoose from "mongoose";
+import { Monofett } from "next/font/google";
 
+type ConnectionObject = {
+  isConnected?: number;
+};
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already Connected to database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    connection.isConnected =  db.connections[0].readyState;
+    console.log("DB connected Succesfully")
+  } catch (error) {
+    console.log("DB connecction failed",error);
+    process.exit(1);
+  }
+}
+export default dbConnect;
